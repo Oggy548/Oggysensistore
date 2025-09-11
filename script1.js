35fdbbf20d3b3bbc989752f74b1c98dd6764d02a
Oggy548 <oggysensistore@gmail.com>
2025-09-12 02:27:49 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadSuccessPage();
+});
+
+function loadSuccessPage() {
+
+    setTimeout(function () {
+        window.location = "iossuccess.html";
+    }, 3000);
+
+}
