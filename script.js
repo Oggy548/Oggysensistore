4c11cb84d26497b57a220c783caf1bc170a34016
Oggy548 <oggysensistore@gmail.com>
2025-10-06 18:17:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function accessIOS() {
 
 
     const adminusername = "OGGYSENSI";
-    const adminpassword = "eRASEv3SERVog";
+    const adminpassword = "V3acsog";
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -129,6 +129,7 @@ function logout() {
     localStorage.removeItem("showSuccess");
     window.location = "ioscodes.html";
 }
+
 
 
 
