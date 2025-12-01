ee6d9747e6e04264f790a9688162f21876921235
Oggy548 <oggysensistore@gmail.com>
2025-12-01 06:19:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function accessIOS() {
 
 
     const adminusername = "OGGYSENSI";
-    const adminpassword = "ncy766";
+    const adminpassword = "vu686v6";
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -146,6 +146,7 @@ function startCounter() {
         );
     });
 }
+
 
 
 
