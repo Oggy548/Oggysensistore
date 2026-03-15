c7e743c9bb6f944ad6b0b188ec19dc7d0fd1b220
Oggy548 <oggysensistore@gmail.com>
2026-03-16 01:31:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ function accessIOS() {
 
 
     const adminusername = "OGGYSENSI";
-    const adminpassword = "V088noy";
+    const adminpassword = "SEN0874y";
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
